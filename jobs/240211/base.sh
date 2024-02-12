8243f7273cd44c09f450544e8420cc6369a00814
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2024-02-12 15:28:10 +0900

    added requirements.txt and made minor modifications

--- jobs/240211/base.sh
+++ jobs/240211/base.sh
@@ -4,16 +4,15 @@ version=$1
 exp_yaml=$1
 ################################
 project_dir="/data/group1/z44722r/project_tmp"
-repo="PLTensorboardTemplete"
+repo="PLTBTemplete"
 num_workers=6
 ################################
 
 echo "num_workers was set to ${num_workers}"
 echo "project_dir was set to ${project_dir}"
-# source "${project_dir}/venv/bin/activate"
-source "/data/group1/z44722r/project02/venv/bin/activate"
-job_dir_name=$(basename "$(dirname "$(pwd)")")
-exp_root="${project_dir}/${repo}/results/${job_dir_name}"
+source "${project_dir}/${repo}/venv/bin/activate"
+# source "/data/group1/z44722r/project02/venv/bin/activate"
+exp_root="${project_dir}/${repo}/results"
 echo ${exp_root}
 cd "${project_dir}/${repo}"
 
